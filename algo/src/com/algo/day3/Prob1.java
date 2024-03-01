4b812e70ae6999020bb520f707a60ae9ccc83c0d
kidchang93 <lck930721@gmail.com>
2024-03-01 18:09:02 +0900

    행 우선 순회 , 열 우선 순회

---
+++ algo/src/com/algo/day3/Prob1.java
@@ -0,0 +1,12 @@
+package com.algo.day3;
+
+public class Prob1 {
+    public static void main(String[] args) {
+        int[][] matrix = {{3, 7, 9}, {4, 2, 6}, {8, 1, 5}};
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                System.out.print(matrix[r][c]); // 379426815
+            }
+        }
+    }
+}
