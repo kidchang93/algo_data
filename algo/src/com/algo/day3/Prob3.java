6b7013ee7a7776581ee607afc6d685b04c038924
kidchang93 <lck930721@gmail.com>
2024-03-01 19:31:50 +0900

    전치

--- algo/src/com/algo/day3/Prob3.java
+++ algo/src/com/algo/day3/Prob3.java
@@ -13,9 +13,9 @@ public class Prob3 {
     }
 
     public static void main(String[] args) {
-        for (int r = 0; r < 3; r++) {
+        for (int r = 0; r < 3; r++) {       // 행 우선 순회
             for (int c = 0; c < 3; c++) {
-                if (r > c) { // 이 경우만
+                if (r > c) { // 이 경우만        or r < c ? 가능  // 둘 중 하나에서는 꼭 조건을 줘야한다.
                     swap(r, c); // 스왑 진행
                 }
             }
